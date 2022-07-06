926390008c015cd6c171e55b5e72e9e1465b51f0
dailyjudge <jmk961202@gmail.com>
2022-07-06 17:04:51 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Box from "./components/Box";
+import { useState } from "react";
 
+const choice = {
+  rock: {
+    name: "Rock",
+    img: "https://nationaltoday.com/wp-content/uploads/2021/08/National-Pet-Rock-Day-640x514.jpg",
+  },
+  scissors: {
+    name: "Scissors",
+    img: "http://iwings.co.kr/web/product/big/202009/b4a485069b64e932431906d79b71cbf0.jpg",
+  },
+  paper: {
+    name: "Paper",
+    img: "https://t1.daumcdn.net/cfile/tistory/2509BA3F57358C8D09",
+  },
+};
 function App() {
+  const [userSelect, setUserSelect] = useState("");
+  const [computerSelect, setComputerSelect] = useState("");
+  const [result, setResult] = useState("");
+  const [comResult, setComResult] = useState("");
+
+  const play = (userChoice) => {
+    setUserSelect(choice[userChoice]);
+
+    let computerChoice = randomChoice();
+    setComputerSelect(computerChoice);
+
+    setResult(judgement_user(choice[userChoice], computerChoice));
+    setComResult(judgement_com);
+  };
+
+  const randomChoice = () => {
+    let itemArray = Object.keys(choice);
+    let randomItem = Math.floor(Math.random() * itemArray.length);
+    let final = itemArray[randomItem];
+
+    return choice[final];
+  };
+
+  const judgement_user = (user, com) => {
+    if (user.name === com.name) {
+      return "Draw";
+    } else if (user.name === "Rock")
+      return com.name === "Scissors" ? "Win" : "Lose";
+    else if (user.name === "Scissors")
+      return com.name === "Paper" ? "Win" : "Lose";
+    else if (user.name === "Paper") return com.name === "Rock" ? "Win" : "Lose";
+  };
+
+  const judgement_com = (resultOfUser) => {
+    if (resultOfUser == "Draw") return "Draw";
+    else if (resultOfUser == "Win") return "Lose";
+    else return "Win";
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="main">
+        <Box title="You" item={userSelect} result={result} />
+        <Box title="Computer" item={computerSelect} result={comResult} />
+      </div>
+      <div className="main">
+        <button onClick={() => play("scissors")}>가위</button>
+        <button onClick={() => play("rock")}>바위</button>
+        <button onClick={() => play("paper")}>보</button>
+      </div>
     </div>
   );
 }
---
+++ src/components/Box.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Box = ({ title, item, result }) => {
+  return (
+    <div className={`${result}`}>
+      <h1>{title}</h1>
+      <img className="item-img" src={item && item.img} />
+      <h2>{result}</h2>
+    </div>
+  );
+};
+
+export default Box;
